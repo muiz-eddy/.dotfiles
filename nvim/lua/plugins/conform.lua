17bdbdbecaddcb84e8beedc886fcbd9f4031c615
Muiz-Eddy <109058675+Muiz-Eddy@users.noreply.github.com>
2023-12-15 22:06:18 +0800

    Added: Conform formatter

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,26 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		-- Define formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettierd" },
+			php = { "php-cs-fixer" },
+		},
+		--Set up format-on-save
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+	},
+}
