54993220722722eca5d5c0777b57b18f74de0811
Muiz-Eddy <109058675+Muiz-Eddy@users.noreply.github.com>
2024-01-02 09:56:15 +0800

    Fix: set the ui on start

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,4 +1,5 @@
 require("config.keymaps")
+require("config.ui")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
--- nvim/lua/config/ui.lua
+++ nvim/lua/config/ui.lua
@@ -1,7 +1,14 @@
-vim.opt.number = true
-vim.opt.textwidth = 80
-vim.opt.colorcolumn = "80"
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.softtabstop = 4
+local opt = vim.opt
+
+-- show line number
+opt.number = true
+
+-- show text-width to 80
+opt.textwidth = 80
+opt.colorcolumn = "80"
+
+-- tab width
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.softtabstop = 4
