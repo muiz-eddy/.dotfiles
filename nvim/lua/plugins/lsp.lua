b9ba913369fa780dadfd8db660dd946d6c67cff8
muiz-eddy <am.eddy25@gmail.com>
2024-02-19 07:45:51 +0800

    Added: new plugins

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -40,6 +40,10 @@ return{
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
           ['<C-f>'] = cmp_action.luasnip_jump_forward(),
           ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          -- Confirm the selected item
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm with Enter key
         })
       })
     end
@@ -62,7 +66,31 @@ return{
       lsp_zero.on_attach(function(client, bufnr)
         -- see :help lsp-zero-keybindings
         -- to learn the available actions
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap=true, silent=true })
         lsp_zero.default_keymaps({buffer = bufnr})
+        vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+          vim.lsp.diagnostic.on_publish_diagnostics,
+          {
+            virtual_text = false,
+            signs = true,
+            update_in_insert = false,
+            underline = true,
+          }
+        )
+        local signs = { Error = "❌", Warn = "⚠️", Hint = "💡", Info = "ℹ️" }
+        for type, icon in pairs(signs) do
+          local hl = "DiagnosticSign" .. type
+          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+
+        vim.cmd [[highlight DiagnosticUnderlineError gui=undercurl guisp=#FF0000]]
+        vim.cmd [[highlight DiagnosticUnderlineWarn gui=underline guisp=#FFDD00]]
+        vim.cmd [[highlight DiagnosticUnderlineInfo gui=underline guisp=#00DDFF]]
+        vim.cmd [[highlight DiagnosticUnderlineHint gui=underline guisp=#00FF00]]        
+        vim.api.nvim_set_keymap(
+          'n', '<Leader>d', ':lua vim.diagnostic.open_float()<CR>', 
+          { noremap = true, silent = true }
+        )
       end)
 
       require('mason-lspconfig').setup({
