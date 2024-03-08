de97e7ec1ee12479d1eff6ddc71739d14d9e7a75
muiz-eddy <am.eddy25@gmail.com>
2024-03-08 21:00:48 +0800

    Change keybind to harpoon and line number to using relative number

--- nvim/lua/config/ui.lua
+++ nvim/lua/config/ui.lua
@@ -1,7 +1,8 @@
 local opt = vim.opt
 
 -- show line number
-opt.number = true
+-- opt.number = true
+opt.relativenumber = true
 
 -- show text-width to 80
 opt.textwidth = 120
--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
 		harpoon : setup()
 		vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-		vim.keymap.set("n", "<C-a>", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
 		vim.keymap.set("n", "<C-s>", function() harpoon:list():select(2) end)
 		vim.keymap.set("n", "<C-d>", function() harpoon:list():select(3) end)
 		vim.keymap.set("n", "<C-f>", function() harpoon:list():select(4) end)
