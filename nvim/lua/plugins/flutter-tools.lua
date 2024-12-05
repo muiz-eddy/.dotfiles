49b5f38b6d80eb2bc37cee0dc6edd1dcbbc39b59
muiz-eddy <109058675+muiz-eddy@users.noreply.github.com>
2024-12-05 13:20:10 +0800

    Added flutter debugger

--- nvim/lua/plugins/flutter-tools.lua
+++ nvim/lua/plugins/flutter-tools.lua
@@ -1,33 +1,41 @@
--- return {
---   'akinsho/flutter-tools.nvim',
---   lazy = false,
---   dependencies = {
---     'nvim-lua/plenary.nvim',
---     'stevearc/dressing.nvim', 
---   },
---   config = function()
---     local dap = require('dap')
---     require('flutter-tools').setup {
---       debugger = {
---         enabled = true,
---         run_via_dap = true,
---         register_configurations = function(_)
---           -- dap.adapters.dart = {
---           --   type = "executable",
---           --   command = "dart",
---           --   args = { "debug_adapter" },
---           -- }
---           --
---           -- dap.adapters.flutter = {
---           --   type = "executable",
---           --   command = "flutter",
---           --   args = { "debug_adapter" },
---           -- }
---           require("dap").configurations.dart = {}
---           require("dap.ext.vscode").load_launchjs() 
---         end,
---       },
---     }
---     vim.keymap.set('n', '<leader>r', require('telescope').extensions.flutter.commands, { desc = 'Open command Flutter' })
---   end,
--- }
+return {
+  'akinsho/flutter-tools.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim', 
+  },
+  config = function()
+    local dap = require('dap')
+    require('flutter-tools').setup {
+      outline = {
+        open_cmd = "0vw",
+        auto_open = false
+      },
+      dev_log = {
+        enabled= false
+      },
+      debugger = {
+        enabled = true,
+        run_via_dap = true,
+        register_configurations = function(_)
+          -- dap.adapters.dart = {
+          --   type = "executable",
+          --   command = "dart",
+          --   args = { "debug_adapter" },
+          -- }
+          --
+          -- dap.adapters.flutter = {
+          --   type = "executable",
+          --   command = "flutter",
+          --   args = { "debug_adapter" },
+          -- }
+          require("dap").configurations.dart = {}
+          require("dap.ext.vscode").load_launchjs() 
+
+        end,
+      },
+    }
+    vim.keymap.set('n', '<leader>r', require('telescope').extensions.flutter.commands, { desc = 'Open command Flutter' })
+  end,
+}
